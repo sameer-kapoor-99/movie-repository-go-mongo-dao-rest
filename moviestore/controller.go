fe273f1fed926a79683f42f1700a7932dc18911a
Sameer Kapoor <sameerkapoor@gmail.com>
2017-11-08 10:26:23 -0800

    DAO is now passed to router externally

--- moviestore/controller.go
+++ moviestore/controller.go
@@ -2,42 +2,43 @@ package moviestore
 
 import (
 	"encoding/json"
-	"io"
-	"io/ioutil"
+
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"../dao/factory"
+	"../dao/interfaces"
 	"../models"
 )
 
 //Controller ...
-type Controller struct {}
+type Controller struct {
+	dao interfaces.UserDao
+}
 
-var db = factory.FactoryDao("mongodb")
+
 // Index GET /
 func (c *Controller) Index(w http.ResponseWriter, r *http.Request) {
 
-	movies, err := db.GetAll() // list of all movies
+	movies, err := c.dao.GetAll() // list of all movies
 	if err != nil {
 		log.Println("Error Index",err)
 	}
 	log.Println(movies)
-	data, _ := json.Marshal(movies)
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
+
+	json.NewEncoder(w).Encode(movies)
 	return
 }
 
 // AddMovie POST /
 func (c *Controller) AddMovie(w http.ResponseWriter, r *http.Request) {
 
+
+	log.Println("in AddMovie")
 	var movie models.Movie
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576)) // read the body of the request
-	log.Println(body)
+	err := json.NewDecoder(r.Body).Decode(&movie)
+
+	log.Println(movie.ID)
 	if err != nil {
 		log.Println("Error AddMovie", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -46,16 +47,9 @@ func (c *Controller) AddMovie(w http.ResponseWriter, r *http.Request) {
 	if err := r.Body.Close(); err != nil {
 		log.Println("Error AddMovie", err)
 	}
-	if err := json.Unmarshal(body, &movie); err != nil { // unmarshall body contents as a type Candidate
-		w.WriteHeader(422) // unprocessable entity
-		if err := json.NewEncoder(w).Encode(err); err != nil {
-			log.Println("Error AddMovie unmarshalling data", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	}
+
 	 // adds the movie to the DB
-	if err := db.AddMovie(movie); err != nil {
+	if err := c.dao.AddMovie(movie); err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -69,7 +63,7 @@ func (c *Controller) AddMovie(w http.ResponseWriter, r *http.Request) {
 // UpdateMovie UPDATE /
 func (c *Controller) UpdateMovie(w http.ResponseWriter, r *http.Request) {
 	var movie models.Movie
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576)) // read the body of the request
+	err := json.NewDecoder(r.Body).Decode(&movie)
 	if err != nil {
 		log.Println("Error UpdateAlbum", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -78,18 +72,9 @@ func (c *Controller) UpdateMovie(w http.ResponseWriter, r *http.Request) {
 	if err := r.Body.Close(); err != nil {
 		log.Println("Error UpdateMovie", err)
 	}
-	if err := json.Unmarshal(body, &movie); err != nil { // unmarshall body contents as a type Candidate
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(422) // unprocessable entity
-		if err := json.NewEncoder(w).Encode(err); err != nil {
-			log.Println("Error UpdateMovie unmarshalling data", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	}
 
 	 // adds the movie to the DB
-	if err := db.UpdateMovie(movie); err != nil {
+	if err := c.dao.UpdateMovie(movie); err != nil {
 		log.Println("Error UpdateMovie updating db", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -106,7 +91,7 @@ func (c *Controller) DeleteMovie(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"] // param id
 
-	if err := db.DeleteMovie(id); err != nil { // delete a album by id
+	if err := c.dao.DeleteMovie(id); err != nil { // delete a album by id
 		log.Println("Error DeleteAlbum",err)
 		w.WriteHeader(http.StatusNotFound)
 		return
@@ -123,19 +108,14 @@ func (c *Controller) DeleteMovie(w http.ResponseWriter, r *http.Request) {
 func (c *Controller) GetMovieByTitle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	title := vars["title"]
-
-	movie,err := db.GetByTitle(title)
+	movie,err := c.dao.GetByTitle(title)
 	if err != nil {
 		log.Println("Error GetMovieByTitle", err)
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 	log.Println(movie)
-	data, _ := json.Marshal(movie)
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
+	json.NewEncoder(w).Encode(movie)
 
 	return
 }
--- moviestore/router.go
+++ moviestore/router.go
@@ -3,9 +3,10 @@ package moviestore
 import (
 	"net/http"
 	"github.com/gorilla/mux"
+	"../dao/interfaces"
 )
 
-var controller = &Controller{}
+
 
 // Route defines a route
 type Route struct {
@@ -18,41 +19,45 @@ type Route struct {
 // Routes defines the list of routes of our API
 type Routes []Route
 
-var routes = Routes{
-	Route{
-		"Index",
-		"GET",
-		"/",
-		controller.Index,
-	},
-	Route{
-		"AddMovie",
-		"POST",
-		"/addmovie/{id}",
-		controller.AddMovie,
-	},
-	Route{
-		"UpdateMovie",
-		"PUT",
-		"/",
-		controller.UpdateMovie,
-	},
-	Route{
-		"DeleteMovie",
-		"DELETE",
-		"/delete/{id}",
-		controller.DeleteMovie,
-	},
-	Route{
-		"GetMovieByTitle",
-		"GET",
-		"/title/{title}",
-		controller.GetMovieByTitle,
-	},
-}
+
 
 //NewRouter configures a new router to the API
-func NewRouter() *mux.Router {
+func NewRouter(dao interfaces.UserDao) *mux.Router {
+	var controller = &Controller{dao : dao}
+
+	var routes = Routes{
+		Route{
+			"Index",
+			"GET",
+			"/movies/",
+			controller.Index,
+		},
+		Route{
+			"AddMovie",
+			"POST",
+			"/movies/",
+			controller.AddMovie,
+		},
+		Route{
+			"UpdateMovie",
+			"PUT",
+			"/movies/{id}",
+			controller.UpdateMovie,
+		},
+		Route{
+			"DeleteMovie",
+			"DELETE",
+			"/movies/{id}",
+			controller.DeleteMovie,
+		},
+		Route{
+			"GetMovieByTitle",
+			"GET",
+			"/movies/title/{title}",
+			controller.GetMovieByTitle,
+		},
+	}
+
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
 		var handler http.Handler
